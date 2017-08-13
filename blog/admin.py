7fa9b630ca76b445f1451cef72de8055db39cc8c
lmd <mingde.liu@intelligentgroup.cn>
2017-08-13 23:16:10 +0800

    Update for first page that Use models show message in Databases

--- blog/admin.py
+++ blog/admin.py
@@ -4,3 +4,12 @@ from __future__ import unicode_literals
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import Post, Category, Tag
+
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['title', 'created_time', 'modified_time', 'category', 'author']
+
+admin.site.register(Post, PostAdmin)
+admin.site.register(Category)
+admin.site.register(Tag)
