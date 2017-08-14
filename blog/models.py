9f0336c6e33348351211235167b257d23fd8dac4
DeMingLiu <937816750@qq.com>
2017-08-14 15:41:03 +0800

    Add show body when click on button

--- blog/models.py
+++ blog/models.py
@@ -3,7 +3,9 @@ from __future__ import unicode_literals
 
 from django.db import models
 from django.contrib.auth.models import User
+from django.urls import reverse
 # Create your models hefrom django.contrib.auth.
+from django.utils.six import python_2_unicode_compatible
 
 # @python_2_unicode_compatible
 class Category(models.Model):
@@ -21,6 +23,10 @@ class Tag(models.Model):
 class Post(models.Model):
     def __unicode__(self):
         return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:detail', kwargs={'pk': self.pk })
+
     title = models.CharField(max_length = 100 )
     body = models.TextField()
     created_time = models.DateTimeField()
--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,10 @@
 # fname: lobg/urls.py
 
 from django.conf.urls import url
-
 from . import views
+
+app_name = 'blog'
 urlpatterns = [
     url(r'^$', views.index, name='index'),
-    # url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
+    url(r'^post/(?P<pk>[0-9]+)/$', views.detail, name='detail'),
 ]
